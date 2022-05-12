1e740c91cdf470d59aee96270d2cae57573ad7f5
Ezy93 <giordani001@gmail.com>
2022-05-12 16:21:33 +0200

    aggiunta card ma senza ancora i dati (problemi con bootstrap)

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 
+
 @section('title', 'movies')
 
 @section('main-content')
@@ -8,6 +9,16 @@
             <div class="row">
                 <div class="col-12">
                     <h1>Movies:</h1>
+                    <div class="col-3">
+                        <div class="card border-dark mb-3">
+                            <div class="card-header bg-transparent border-dark">Header</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Success card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                            <div class="card-footer bg-transparent border-success">Footer</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
