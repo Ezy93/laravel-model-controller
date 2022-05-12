6f4493337afc439aba97a8137053174d39bfd131
Ezy93 <giordani001@gmail.com>
2022-05-12 16:55:48 +0200

    aggiunte dinamicamente le card dei movies prendendo i dati dal database

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -8,17 +8,25 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <h1>Movies:</h1>
-                    <div class="col-3">
-                        <div class="card border-dark mb-3">
-                            <div class="card-header bg-transparent border-dark">Header</div>
-                            <div class="card-body">
-                                <h5 class="card-title">Success card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h1 class="my-4">Movies:</h1>
+                    <div class="row">
+                        @foreach ($movies as $movie)
+                            <div class="col-3">
+                                <div class="card border-dark mb-3">
+                                    <div class="card-header bg-transparent border-dark"><span class="fw-bold">Movie id:</span>{{" ".$movie->id}}</div>
+                                    <div class="card-body">
+                                        <h5 class="card-title fw-bolder mb-3">Movie info:</h5>
+                                        <p class="card-text"> <span class="fw-bold">Title:</span> {{' '.$movie->title}}</p>
+                                        <p class="card-text"> <span class="fw-bold">Original title:</span> {{' '.$movie->original_title}}</p>
+                                        <p class="card-text"> <span class="fw-bold">Nation of production:</span> {{' '.$movie->nationality}}</p>
+                                        <p class="card-text"> <span class="fw-bold">Release date:</span> {{' '.$movie->date}}</p>
+                                    </div>
+                                    <div class="card-footer bg-transparent border-success"><span class="fw-bold">Rating:</span>{{' '.$movie->vote}}</div>
+                                </div>
                             </div>
-                            <div class="card-footer bg-transparent border-success">Footer</div>
-                        </div>
+                        @endforeach
                     </div>
+                        
                 </div>
             </div>
         </div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
-<footer>
-    <div>
-        <h3>Footer placeholder</h3>
+<footer class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">footer</a>
     </div>
 </footer>
